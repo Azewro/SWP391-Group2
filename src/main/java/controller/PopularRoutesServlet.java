e8bd5e4ccad72b95823c45726c92050755623feb
kuro2307 <ktlegend201@gmail.com>
2025-03-04 00:18:32 +0700

    done popularroutes

--- src/main/java/controller/PopularRoutesServlet.java
+++ src/main/java/controller/PopularRoutesServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import model.Route;
 
 @WebServlet(name="PopularRoutesServlet", urlPatterns="/home")
@@ -17,27 +18,14 @@ public class PopularRoutesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
         PopularRoutesDAO popularRoutesDAO = new PopularRoutesDAO();
         try {
-            ArrayList<Route> popularRoutes = popularRoutesDAO.getPopularRoutes();
-            if (popularRoutes == null || popularRoutes.isEmpty()) {
-                System.out.println("Không có tuyến đường phổ biến nào được tìm thấy!");
-            } else {
-                for (Route route : popularRoutes) {
-                    System.out.println(route.getRouteName() + " từ "
-                            + route.getStartLocation().getName()
-                            + " đến "
-                            + route.getEndLocation().getName()
-                            + ", Giá vé: " + route.getBasePrice() + " VNĐ");
-                }
-            }
+            List<Route> popularRoutes = popularRoutesDAO.getPopularRoutes();
             request.setAttribute("popularRoutes", popularRoutes);
             request.getRequestDispatcher("index.jsp").forward(request, response);
         } catch (SQLException e) {
             e.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unable to load popular routes.");
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error fetching popular routes.");
         }
     } 
 
--- src/main/java/dao/PopularRoutesDAO.java
+++ src/main/java/dao/PopularRoutesDAO.java
@@ -12,49 +12,44 @@ import model.Route;
 import util.DatabaseConnection;
 
 public class PopularRoutesDAO {
-    public ArrayList<Route> getPopularRoutes() throws SQLException {
+    public List<Route> getPopularRoutes() throws SQLException {
         List<Route> routes = new ArrayList<>();
-        String query = "SELECT r.route_id, r.route_name, r.start_location_id, r.end_location_id, r.distance, r.estimated_duration, c.city_name AS start_city, l2.name AS end_city, r.base_price\n" +
+        String query = "SELECT \n" +
+"    c.city_name AS start_location,  -- Cần đặt alias đúng\n" +
+"    r.route_id, \n" +
+"    r.route_name, \n" +
+"    l2.name AS end_location, \n" +
+"    r.distance, \n" +
+"    r.estimated_duration, \n" +
+"    r.base_price\n" +
 "FROM Routes r\n" +
-"JOIN Locations l ON r.start_location_id = l.location_id\n" +
-"JOIN Cities c ON l.address = c.city_name\n" +
+"JOIN Locations l1 ON r.start_location_id = l1.location_id\n" +
 "JOIN Locations l2 ON r.end_location_id = l2.location_id\n" +
-"JOIN (\n" +
-"   SELECT city_name\n" +
-" FROM Cities\n" +
-" LEFT JOIN Locations ON Cities.city_name = Locations.address\n" +
-"   LEFT JOIN Routes ON Routes.start_location_id = Locations.location_id\n" +
-"    GROUP BY city_name\n" +
-"ORDER BY COUNT(Routes.route_id) DESC\n" +
-" LIMIT 3\n" +
-") AS popularCities ON c.city_name = popularCities.city_name\n" +
-"ORDER BY c.city_name, r.estimated_duration\n" +
-"LIMIT 3";
+"JOIN Cities c ON l1.address = c.city_name";
 
         try (Connection conn = DatabaseConnection.getConnection();
-                PreparedStatement ps = conn.prepareStatement(query);
-                ResultSet rs = ps.executeQuery()) {
+             PreparedStatement ps = conn.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
-    Route route = new Route();
-    route.setRouteId(rs.getInt("route_id"));
-    route.setRouteName(rs.getString("route_name"));
+                Route route = new Route();
+                route.setRouteId(rs.getInt("route_id"));
+                route.setRouteName(rs.getString("route_name"));
 
-    Location startLocation = new Location();
-    startLocation.setName(rs.getString("start_city"));
-    route.setStartLocation(startLocation);
+                Location startLoc = new Location();
+                startLoc.setName(rs.getString("start_location"));
+                route.setStartLocation(startLoc);
 
-    Location endLocation = new Location();
-    endLocation.setName(rs.getString("end_city"));
-    route.setEndLocation(endLocation);
+                Location endLoc = new Location();
+                endLoc.setName(rs.getString("end_location"));
+                route.setEndLocation(endLoc);
 
-    route.setDistance(rs.getFloat("distance"));
-    route.setEstimatedDuration(rs.getInt("estimated_duration"));
-    route.setBasePrice(rs.getBigDecimal("base_price"));
+                route.setDistance(rs.getFloat("distance"));
+                route.setEstimatedDuration(rs.getInt("estimated_duration"));
+                route.setBasePrice(rs.getBigDecimal("base_price"));
 
-    routes.add(route);
-}
-
+                routes.add(route);
+            }
         }
-        return (ArrayList<Route>) routes;
+        return routes;
     }
 }
